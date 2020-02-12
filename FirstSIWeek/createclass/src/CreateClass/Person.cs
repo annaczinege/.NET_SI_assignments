4bc9ecdbbf365926fff4e9c645e772ed0138f032
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-02-12 20:34:21 +0100

    Add CreateClass project and Person.cs

---
+++ FirstSIWeek/createclass/src/CreateClass/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreateClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
