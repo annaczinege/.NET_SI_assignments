59a3f7e652435126aa672460b6cbc1d41df06576
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-02-13 12:23:14 +0100

    Create SortString project

---
+++ FirstSIWeek/sortstring/src/SortString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
