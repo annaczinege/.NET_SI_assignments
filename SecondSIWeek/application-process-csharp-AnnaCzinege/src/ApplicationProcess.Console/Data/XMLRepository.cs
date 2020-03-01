fda94ad9da342a944cf769b4aefeee5739da70f1
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-03-01 17:40:59 +0100

    Update XMLRepository.cs

--- SecondSIWeek/application-process-csharp-AnnaCzinege/src/ApplicationProcess.Console/Data/XMLRepository.cs
+++ SecondSIWeek/application-process-csharp-AnnaCzinege/src/ApplicationProcess.Console/Data/XMLRepository.cs
@@ -34,6 +34,7 @@ namespace Codecool.ApplicationProcess.Data
         /// <inheritdoc/>
         public IEnumerable<Mentor> GetAllMentorFrom(City city)
         {
+            // serialization
             var mentors = _doc.Descendants("Mentors").Descendants("Mentor");
             var mentorsFromCity = new HashSet<Mentor>();
             foreach (var mentor in mentors)
@@ -55,6 +56,7 @@ namespace Codecool.ApplicationProcess.Data
         /// <inheritdoc/>
         public IEnumerable<Mentor> GetAllMentorWhomFavoriteLanguage(string language)
         {
+            // serialization
             var mentors = _doc.Descendants("Mentors").Descendants("Mentor");
             var mentorsOfLanguage = new List<Mentor>();
             foreach (var mentor in mentors)
@@ -76,43 +78,9 @@ namespace Codecool.ApplicationProcess.Data
         /// <inheritdoc/>
         public IEnumerable<Applicant> GetApplicantsOf(string contactMentorName)
         {
-            var mentors = _doc.Descendants("Applications").Descendants("Application").Descendants("Mentor");
-            var applicants = _doc.Descendants("Applications").Descendants("Application").Descendants("Applicant");
-
-            var mentorList = new List<Mentor>();
-            var applicantList = new List<Applicant>();
-            var applicantList2 = new List<Applicant>();
-
-            foreach (var mentor in mentors)
-            {
-                Mentor ment = new Mentor();
-                StringReader reader = new StringReader(mentor.ToString());
-                XmlSerializer serializer = new XmlSerializer(typeof(Mentor));
-                ment = (Mentor)serializer.Deserialize(reader);
-                mentorList.Add(ment);
-            }
-
-            foreach (var applicant in applicants)
-            {
-                Applicant appl = new Applicant();
-                StringReader reader = new StringReader(applicant.ToString());
-                XmlSerializer serializer = new XmlSerializer(typeof(Applicant));
-                appl = (Applicant)serializer.Deserialize(reader);
-                applicantList.Add(appl);
-            }
-
-            for (int i = 0; i < mentorList.Count; i++)
-            {
-                if (mentorList[i].FirstName.Equals(contactMentorName)
-                    || mentorList[i].LastName.Equals(contactMentorName)
-                    || mentorList[i].LastName.Equals(contactMentorName)
-                    || mentorList[i].Nickname.Equals(contactMentorName))
-                {
-                    applicantList2.Add(applicantList[i]);
-                }
-            }
-
-            return applicantList2;
+            return _doc.Descendants("Applications").Descendants("Application")
+                .Where(application => contactMentorName.Equals(application.Element("Mentor").Element("Nickname").Value))
+                .Select(application => CreateNewApplicant(application.Element("Applicant")));
         }
 
         /// <inheritdoc/>
@@ -121,6 +89,21 @@ namespace Codecool.ApplicationProcess.Data
             return _doc.Descendants("Applicants").Descendants("Applicant")
                 .Where(applicant => applicant.Element("Status").Value == "Applied")
                 .Select(applicant => applicant.Element("Email").Value).ToList();
+        }
+
+        private Applicant CreateNewApplicant(XElement element)
+        {
+            return new Applicant()
+            {
+                Id = (int)element.Element("Id"),
+                FirstName = (string)element.Element("FirstName"),
+                LastName = (string)element.Element("LastName"),
+                PhoneNumber = (string)element.Element("PhoneNumber"),
+                Email = (string)element.Element("Email"),
+                ApplicationCode = (int)element.Element("ApplicationCode"),
+                Status = (ApplicationStatus)Enum.Parse(typeof(ApplicationStatus), element.Element("Status").Value),
+                StartDate = element.Element("StartDate").IsEmpty ? default : (DateTime)element.Element("StartDate"),
+            };
         }
     }
 }
