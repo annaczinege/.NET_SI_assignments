82084d114f3e7d76e3104027d39106019b79cc33
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-03-09 21:28:55 +0100

    Add Header to the list

--- ThirdSIWeek/todo/src/App.js
+++ ThirdSIWeek/todo/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Todo from "./components/Todo";
+import Header from "./components/layout/Header";
 import "./App.css";
 
 class App extends Component {
@@ -45,6 +46,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Todo
           todos={this.state.todos}
           markComplete={this.markComplete}
---
+++ ThirdSIWeek/todo/src/components/AddTodo.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class AddTodo extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default AddTodo;
