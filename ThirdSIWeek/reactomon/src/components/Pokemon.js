c91a17cfb8f2a433513be50d980a23139255184b
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-03-13 14:37:07 +0100

    Rewrite Pokemon.js to functional component

--- ThirdSIWeek/reactomon/src/components/Pokemon.js
+++ ThirdSIWeek/reactomon/src/components/Pokemon.js
@@ -1,46 +1,46 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import Axios from "axios";
 
-export class Pokemon extends Component {
-  state = {
-    front_default: ""
+export const Pokemon = props => {
+  const [imgUrl, setImgUrl] = useState("");
+
+  const Capitalize = str => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
   };
 
-  Capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
-
-  componentDidMount() {
-    const pokeUrl = this.props.pokemon.url;
+  const fetchImage = () => {
+    const pokeUrl = props.pokemon.url;
     Axios.get(`${pokeUrl}`).then(res =>
-      this.setState({ front_default: res.data.sprites.front_default })
+      setImgUrl(res.data.sprites.front_default)
     );
-  }
+  };
 
-  render() {
-    let { name } = this.props.pokemon;
-    name = this.Capitalize(name);
-    return (
-      <div className="card">
-        <img src={`${this.state.front_default}`} alt=""></img>
-        <div>
-          <Link
-            to={{
-              pathname: `/pokemon/${this.props.pokemon.name}`,
-              state: {
-                url: this.props.pokemon.url
-              }
-            }}
-          >
-            {name}
-          </Link>
-        </div>
+  useEffect(() => {
+    fetchImage();
+  });
+
+  let name = props.pokemon.name;
+  name = Capitalize(name);
+  return (
+    <div className="card">
+      <img src={`${imgUrl}`} alt=""></img>
+      <div>
+        <Link
+          to={{
+            pathname: `/pokemon/${props.pokemon.name}`,
+            state: {
+              url: props.pokemon.url
+            }
+          }}
+        >
+          {name}
+        </Link>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Pokemon.propTypes = {
   pokemon: PropTypes.object.isRequired
