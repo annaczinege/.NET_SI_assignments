4c7aff20a1e870474f5f1f572fc7e8f33eeddb94
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-03-13 14:40:34 +0100

    Rewrite PokemonList.js to functional component

--- ThirdSIWeek/reactomon/src/components/pages/PokemonList.js
+++ ThirdSIWeek/reactomon/src/components/pages/PokemonList.js
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import Pokemon from "../Pokemon";
 import PropTypes from "prop-types";
 
-export class PokemonList extends Component {
-  render() {
-    return this.props.pokemons.map(pokemon => (
-      <div className="pokemon-names card">
-        <Pokemon pokemon={pokemon} />
-      </div>
-    ));
-  }
-}
+export const PokemonList = props => {
+  return props.pokemons.map(pokemon => (
+    <div className="pokemon-names card">
+      <Pokemon pokemon={pokemon} />
+    </div>
+  ));
+};
 
 PokemonList.propTypes = {
   pokemons: PropTypes.array.isRequired
