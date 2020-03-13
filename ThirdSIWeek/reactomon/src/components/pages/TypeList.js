068ec4db4b7565c10d68c2aac0d7044981dd2ac5
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-03-13 14:38:48 +0100

    Rewrite TypeList.js to functional component

--- ThirdSIWeek/reactomon/src/components/pages/TypeList.js
+++ ThirdSIWeek/reactomon/src/components/pages/TypeList.js
@@ -1,12 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import Type from "../Type";
 import PropTypes from "prop-types";
 
-export class TypeList extends Component {
-  render() {
-    return this.props.types.map(type => <Type key={type.id} type={type} />);
-  }
-}
+export const TypeList = props => {
+  return props.types.map(type => <Type key={type.id} type={type} />);
+};
 
 TypeList.propTypes = {
   types: PropTypes.array.isRequired
