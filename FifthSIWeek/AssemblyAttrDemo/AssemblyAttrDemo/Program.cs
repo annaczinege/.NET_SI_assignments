86f99e68fc57239957e62185a2c59516f7f1c6a3
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-04-08 19:19:54 +0200

    Create AssemblyAttrDemo console project

---
+++ FifthSIWeek/AssemblyAttrDemo/AssemblyAttrDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssemblyAttrDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
