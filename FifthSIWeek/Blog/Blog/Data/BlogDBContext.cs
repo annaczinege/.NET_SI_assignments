d7576129ee2896c3c4c4e29231881e3ba2956a09
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-04-08 17:42:35 +0200

    Add new users by seeding data

--- FifthSIWeek/Blog/Blog/Data/BlogDBContext.cs
+++ FifthSIWeek/Blog/Blog/Data/BlogDBContext.cs
@@ -13,6 +13,7 @@ namespace Blog.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // we could add a ModelBuilderExtension class with a Seed() method to initialize some data
             modelBuilder.Entity<User>().HasData(new User { Id=1, FirstName="Anna", LastName="Czinege"},
                 new User { Id = 2, FirstName = "Eszter", LastName = "Mázi" },
                 new User { Id = 3, FirstName = "Norbert", LastName = "Benkó" });
