ef7286440b7ffb48499d8ce70ba9c59e806fdbf6
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-04-10 17:25:47 +0200

    Finish Exercise5

---
+++ FifthSIWeek/DemoDynamic/DemoDynamic/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace DemoDynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an Assembly Name
+            AssemblyName theName = new AssemblyName();
+            theName.Name = "DemoAssembly";
+            theName.Version = new Version("1.0.0.0");
+
+            // Get the AppDomain to put our assembly in
+            AppDomain domain = AppDomain.CurrentDomain;
+
+            // Create the Assembly
+            AssemblyBuilder assemBldr = domain.De //domain.DefineDynamicAssembly(theName, AssemblyBuilderAccess.ReflectionOnly);
+
+            // Define a module to hold our type
+            ModuleBuilder modBldr = assemBldr.DefineDynamicModule("CodeModule",
+                                                                  "DemoAssembly.dll");
+
+            // Create a new type
+            TypeBuilder animalBldr = modBldr.DefineType("Animal", TypeAttributes.Public);
+
+            // Display the new Type
+            Type animal = animalBldr.CreateType();
+            Console.WriteLine(animal.FullName);
+
+            foreach (MemberInfo info in animal.GetMembers())
+            {
+                Console.WriteLine(" Member ({0}): {1}", info.MemberType, info.Name);
+            }
+        }
+    }
+}
---
+++ FifthSIWeek/DynamicCodeDemo/DynamicCodeDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+namespace DynamicCodeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\System.Web.dll";
+            Assembly webAssembly = Assembly.LoadFrom(path);
+
+            // Get the type to the HttpUtility class
+            Type utilType = webAssembly.GetType("System.Web.HttpUtility");
+
+            // Get the static HtmlEncode and HtmlDecode methods
+            MethodInfo encode = utilType.GetMethod("HtmlEncode", new Type[] { typeof(string) });
+            MethodInfo decode = utilType.GetMethod("HtmlDecode", new Type[] { typeof(string) });
+
+            // Create a string to be encoded
+            string originalString = "This is Sally & Jack's Anniversary <sic>";
+            Console.WriteLine(originalString);
+
+            // encode it and show the encoded value
+            string encoded = (string)encode.Invoke(null, new object[] { originalString });
+            Console.WriteLine(encoded);
+
+            // decode it to make sure it comes back right
+            string decoded = (string)decode.Invoke(null, new object[] { encoded });
+            Console.WriteLine(decoded);
+        }
+    }
+}
