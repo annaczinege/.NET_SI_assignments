b5def0f1f15aaea3880d1aa492e3f16f9e2cb925
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-04-10 16:56:39 +0200

    Finish Exercise3

---
+++ FifthSIWeek/AssemblyDemo2/AssemblyDemo2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\System.ServiceProcess.dll";
+
+            // Using BindingFlags to only get declared and instance members
+            BindingFlags flags = BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance;
+
+            // Load the Assembly from the path
+            Assembly theAssembly = Assembly.LoadFrom(path);
+            Console.WriteLine(theAssembly.FullName);
+            Type[] types = theAssembly.GetTypes();
+            Console.WriteLine(types);
+
+            foreach (Type type in types)
+            {
+                Console.WriteLine(" Type: {0}", type.Name);
+                MemberInfo[] members = type.GetMembers(flags);
+
+                foreach (MemberInfo member in members)
+                {
+                    Console.WriteLine(" {0}: {1}", member.MemberType, member.Name);
+                }
+            }
+
+        }
+    }
+}
