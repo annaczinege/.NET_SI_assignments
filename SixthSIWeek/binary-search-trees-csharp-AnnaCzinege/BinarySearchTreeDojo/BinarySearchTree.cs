26817fabd3542f611b3626a798d948fa10738815
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-04-24 18:42:53 +0200

    Pass Search and Add tests

--- SixthSIWeek/binary-search-trees-csharp-AnnaCzinege/BinarySearchTreeDojo/BinarySearchTree.cs
+++ SixthSIWeek/binary-search-trees-csharp-AnnaCzinege/BinarySearchTreeDojo/BinarySearchTree.cs
@@ -5,29 +5,81 @@ namespace BinarySearchTreeDojo
 {
     public class BinarySearchTree
     {
-        private readonly List<int> _treeElements;
-        private BinarySearchTree(List<int> intList)
+        private Node _rootNode;
+        public Node RootNode { get { return _rootNode; } set { _rootNode = value; } }
+        private BinarySearchTree()
         {
-            _treeElements = intList;
         }
 
         public static BinarySearchTree Build(List<int> elements)
         {
-            BinarySearchTree tree = new BinarySearchTree(elements);
+            BinarySearchTree tree = new BinarySearchTree();
+
+            tree.RootNode = tree.BuildTree(elements, 0, elements.Count - 1);
+
             return tree;
         }
-    
-        public Boolean Search(int toFind) {
-            
+
+        public Boolean Search(int toFind)
+        {
+            if (_rootNode != null)
+            {
+                return _rootNode.Search(toFind);
+            }
             return false;
         }
 
-        public void Add(int toAdd) {
-            // TODO adds an element. Throws an error if it exist.
+        public void Add(int toAdd)
+        {
+            if (_rootNode != null)
+            {
+                _rootNode.Add(toAdd);
+            }
+            else
+            {
+                _rootNode = new Node(toAdd);
+            }
         }
 
-        public void Remove(int toRemove) {
-            // TODO removes an element. Throws an error if its not on the tree.
-        }     
+        public void Remove(int toRemove)
+        {
+            Node currentNode = _rootNode;
+            Node parentNode = _rootNode;
+
+            while (currentNode != null && currentNode.Data != toRemove)
+            {
+                parentNode = currentNode;
+
+                if (toRemove < currentNode.Data)
+                {
+                    currentNode = currentNode.Left;
+                }
+                else
+                {
+                    currentNode = currentNode.Right;
+                }
+            }
+
+            if (currentNode == null)
+            {
+                return;
+            }
+        }
+
+        private Node BuildTree(List<int> listOfNumbers, int startIndex, int endIndex)
+        {
+            if (startIndex > endIndex)
+            {
+                return null;
+            }
+
+            int midIndex = (startIndex + endIndex) / 2;
+            Node node = new Node(listOfNumbers[midIndex]);
+
+            node.Left = BuildTree(listOfNumbers, startIndex, midIndex - 1);
+            node.Right = BuildTree(listOfNumbers, midIndex + 1, endIndex);
+
+            return node;
+        }
     }
 }
---
+++ SixthSIWeek/binary-search-trees-csharp-AnnaCzinege/BinarySearchTreeDojo/Node.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeDojo
+{
+    public class Node
+    {
+        private int _data;
+        private Node _rightNode;
+        private Node _leftNode;
+        private bool _isDeleted;
+        public int Data { get { return _data; } set { _data = value; } }
+        public Node Left { get { return _leftNode; } set { _leftNode = value; } }
+        public Node Right { get { return _rightNode; } set { _rightNode = value; } }
+        public bool IsDeleted { get {return _isDeleted; } }
+
+        public Node(int data)
+        {
+            _data = data;
+            _isDeleted = false;
+        }
+
+        public bool Search(int value)
+        {
+            if (value == _data && IsDeleted == false)
+            {
+                return true;
+            }
+            else if (value < _data && _leftNode != null)
+            {
+                return _leftNode.Search(value);
+            }
+            else if (_rightNode != null)
+            {
+                return _rightNode.Search(value);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Add(int value)
+        {
+            if (value >= _data)
+            {
+                if (_rightNode == null)
+                {
+                    _rightNode = new Node(value);
+                }
+                else
+                {
+                    _rightNode.Add(value);
+                }
+            }
+            else
+            {
+                if (_leftNode == null)
+                {
+                    _leftNode = new Node(value);
+                }
+                else
+                {
+                    _leftNode.Add(value);
+                }
+            }
+        }
+    }
+}
