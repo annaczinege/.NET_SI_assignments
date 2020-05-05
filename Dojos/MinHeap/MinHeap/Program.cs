311cd37ae347468f3aec934c1e58ce886b6634d2
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-05-05 20:31:03 +0200

    Create MinHeap console app

---
+++ Dojos/MinHeap/MinHeap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MinHeap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
